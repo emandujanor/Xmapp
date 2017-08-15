bdcfd70ea8b0e13127f31fb05612cc9bc78a79b3
Anna karina Luz <anna.luz.duran@gmail.com>
2017-08-15 16:37:40 -0500

    imagenes y script firebase

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyACg2z3JQODfTcOfIl97_uUhQkGtWJJJaI",
+    authDomain: "x-app-a4675.firebaseapp.com",
+    databaseURL: "https://x-app-a4675.firebaseio.com",
+    projectId: "x-app-a4675",
+    storageBucket: "x-app-a4675.appspot.com",
+    messagingSenderId: "657424766858"
+  };
+  firebase.initializeApp(config);
+
