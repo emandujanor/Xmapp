b21f428cec497e7b4dae90e6b400cd3f50e3058d
karyvdj <kaina51@hotmail.com>
2017-08-16 13:40:53 -0500

    Arreglando detalle de cartas y footer en busquedda

--- public/js/mostrar.js
+++ public/js/mostrar.js
@@ -1,9 +1,9 @@
-var cargarPagina = function () {    
+var cargarPagina = function () {
     mastraEvnetoF    ();
     obtenerEventos();
     $("#mostrar-busqueda").click(searEvent);
     $("#mostrar-result").click(buscarD);
-	
+
 
 };
 var eventos = [];
@@ -11,15 +11,15 @@ var plantillaEvent =
 '<div class="key-reserva" data-key="__key__">'+
   '<div class="uk-card uk-card-default uk-card-hover">'+
     '<div class="uk-card-header">'+
-        '<div class="uk-grid-small uk-flex-middle" uk-grid>'+
-            '<div class="uk-width-auto">'+
+        '<div class="uk-align-center">'+
+            '<div class="uk-text-center uk-margin-bottom">'+
                 '<img  width="140" height="140" src="__imagen__">'+
             '</div>'+
-            '<div class="uk-width-expand">'+
+            '<div>'+
               '<h3 class="uk-card-title uk-margin-remove-bottom">__nombre__</h3>'+
-              '<p class="uk-text-meta uk-margin-remove-top">$__fecha__</p>'+
+              '<p class="uk-text-meta uk-margin-remove-top uk-margin-remove-bottom">__fecha__</p>'+
             '</div>'+
-        '</div>'+
+         '</div>'+
     '</div>'+
     '<div class="uk-card-body">'+
        ' <p>__descripcion__</p>'+
@@ -34,9 +34,9 @@ var obtenerEventos = function () {
         var activities = data;
         $.each(activities, function(key,val) {
             eventos.push(val);
-        });    
-    });     
-     
+        });
+    });
+
 
 };
 var mostrarEvent = function (events) {
@@ -88,4 +88,3 @@ function buscarD(e) {
 
 
 $(document).ready(cargarPagina);
-
